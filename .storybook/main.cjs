75b3776f0ef2a04dfa87dd86e82b22881243229d
Thadeu <guilherme.horta@flarconsulting.com.br>
2022-10-13 02:04:37 -0300

    deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -16,9 +16,10 @@ module.exports = {
     "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
-    if (configType == 'PRODUCTION') {
+    if (configType === 'PRODUCTION') {
       config.base = '/ignite-lab-design-system/'
     }
+
     return config
   }
 }
