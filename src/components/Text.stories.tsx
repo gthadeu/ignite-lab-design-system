3150d59a8c8c3bac9b44673b1f1b143da84bfcab
Thadeu <guilherme.horta@flarconsulting.com.br>
2022-10-13 02:00:41 -0300

    add: text

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,7 +5,7 @@ export default {
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Lorem ispum.',
+        children: 'Lorem ispum solem.',
         size: 'md',
     },
     argTypes: {
